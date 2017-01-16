b4d81002a9bb140612825bce665f49f604df90d8
nghildebrandt <n.g.hildebrandt@gmail.com>
2017-01-16 18:07:47 +0100

    added lab3 question
    tried to implement a library to make the product inventory table
    sortable but havent succeded yet

---
+++ week1/scriptFunctions.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor (){
+  document.body.style.backgroundColor = "red";
+}
+
+function preventDefaultWebsite (){
+  document.addEventListener("click", function(event){
+    event.preventDefault()
+});
+}
