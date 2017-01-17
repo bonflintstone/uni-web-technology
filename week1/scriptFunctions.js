5b588855a15b26fd5adde509d9c3545ecb850ef7
Bonflintstone <freddy@railslove.com>
2017-01-17 12:30:06 +0100

    Added table sorting and fixed the table in the process

--- week1/scriptFunctions.js
+++ week1/scriptFunctions.js
@@ -1,9 +1,9 @@
-function changeBackgroundColor (){
+function changeBackgroundColor() {
   document.body.style.backgroundColor = "red";
 }
 
-function preventDefaultWebsite (){
-  document.addEventListener("click", function(event){
-    event.preventDefault()
-});
+function preventDefaultWebsite() {
+  document.addEventListener("click", function(event) {
+    event.preventDefault();
+  });
 }
---
+++ week1/tableSort.js
@@ -0,0 +1,28 @@
+addEventListeners = function() {
+  var tableHeaders = document.querySelectorAll('#product-table thead th');
+
+  for (header of tableHeaders) {
+    header.addEventListener('click', function() {
+      var index = Array.from(this.parentNode.children).indexOf(this);
+      sortTableBy(index);
+    }, false);
+  }
+}
+
+sortTableBy = function(index) {
+  var rows = Array.prototype.slice.call(document.querySelectorAll('#product-table tbody tr'));
+  var tbody = document.querySelector('#product-table tbody');
+
+  rows = rows.sort(function(a, b) {
+    aText = a.children[index].innerHTML;
+    bText = b.children[index].innerHTML;
+
+    return bText.localeCompare(aText);
+  });
+
+  for (row of rows.reverse()) {
+    tbody.appendChild(row);
+  }
+}
+
+addEventListeners();
