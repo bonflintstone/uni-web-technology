d1e813ca858f7786b8d5d74910b6981a5576b56c
Bonflintstone <freddy@railslove.com>
2017-01-27 15:36:12 +0100

    Fixed some stuff and set the return status codes right(er)

--- python-framework/server.py
+++ python-framework/server.py
@@ -1,17 +1,17 @@
-from bottle import response, error, get, request, post, delete, put
+from bottle import response, error, get, request, post, delete, put, HTTPResponse
 import json
 
 @get('/items')
 def get_items(db):
-    db.execute("SELECT rowid, name, category, amount, date, location FROM items")
-    names = db.fetchall()
-    return json.dumps(names)
+  db.execute("SELECT rowid, name, category, amount, date, location FROM items")
+  items = json.dumps(db.fetchall())
+  return HTTPResponse(status=200, body=items)
 
-@get('/items/<rowid>')
+@get('/item/<rowid>')
 def get_item(db, rowid=0):
   db.execute('SELECT * FROM items WHERE rowid=?', rowid)
-  name = db.fetchone()
-  return json.dumps(name)
+  item = json.dumps(db.fetchone())
+  return HTTPResponse(status=200, body=item)
 
 @post('/items')
 def post_items(db):
@@ -20,14 +20,14 @@ def post_items(db):
                 VALUES (?, ?, ?, ?, ?)""",
              (item['category'], item['date'], item['amount'], item['name'],  item['location']))
   response.content_type = 'application/json'
-  return json.dumps(response.content_type)
+  return HTTPResponse(status=201, body=None)
 
 @delete('/item/<rowid>')
 def delete_item(db, rowid):
   db.execute("""SELECT * FROM items WHERE rowid=?""", rowid)
   item = db.fetchone()
   db.execute("""DELETE FROM items WHERE rowid=?""", (rowid))
-  return item
+  return HTTPResponse(status=200, body=item)
 
 @put('/item/<rowid>')
 def put_item(db, rowid):
@@ -38,11 +38,11 @@ def put_item(db, rowid):
                 category=?, date=?, amount=?, name=?, location=?
                 WHERE rowid=?""",
              (item['category'], item['date'], item['amount'], item['name'], item['location'], rowid))
+  return HTTPResponse(status=200, body=item)
 
 @error(404)
 def error_404_handler(e):
     response.content_type = 'application/json'
-
     return json.dumps({'Error': {'Message': e.status_line, 'Status': e.status_code}})
 
 if __name__ == "__main__":
