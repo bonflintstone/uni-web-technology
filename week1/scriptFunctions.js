045270db6ba55e69709641e38190d06c07b70077
Bonflintstone <freddy@railslove.com>
2017-02-01 11:42:17 +0100

    Added font size toggle and accessibility report

--- week1/scriptFunctions.js
+++ week1/scriptFunctions.js
@@ -11,5 +11,20 @@ function preventDefaults() {
     event.preventDefault();
   });
 }
-
 preventDefaults();
+
+function addAccessibility() {
+  document.getElementById('toggle-font').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    bodyStyle = document.querySelector('body').style;
+
+    if(bodyStyle.fontSize != '200%') {
+      bodyStyle.fontSize = '200%'
+    } else {
+      bodyStyle.fontSize = '100%'
+    }
+  });
+}
+
+addAccessibility()
